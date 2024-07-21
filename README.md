8e2dda50a429136a3ade9db74532006108acb95e
cialloo <aluckywenda@gmail.com>
2024-07-21 21:41:18 +0800

    chore: readme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # winui_n2n
 
-A new Flutter project.
+**winui_n2n** is an open-source project based on **n2n**, an excellent cross-platform P2P VPN software.
+
+## Features
+
+- Cross-platform support
+- P2P VPN capabilities
+- User-friendly GUI built with Flutter
+
+## Usage
+
+Detailed usage instructions will be added here.
+
+## License
+
+This project is licensed under the terms of the GPL-3.0 license. See the [LICENSE](https://github.com/moemoequte/winui_n2n/blob/main/LICENSE) file for details.
+
+## Author
+
+For more information, please visit the author's website: [Cialloo](https://www.cialloo.com)  
+For any inquiries, contact: [admin@cialloo.com](mailto:admin@cialloo.com)
--- lib/about_page.dart
+++ lib/about_page.dart
@@ -3,7 +3,29 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 const _enContent = '''
-'''; // TODO
+# winui_n2n
+
+**winui_n2n** is an open-source project based on **n2n**, an excellent cross-platform P2P VPN software.
+
+## Features
+
+- Cross-platform support
+- P2P VPN capabilities
+- User-friendly GUI built with Flutter
+
+## Usage
+
+Detailed usage instructions will be added here.
+
+## License
+
+This project is licensed under the terms of the GPL-3.0 license. See the [LICENSE](https://github.com/moemoequte/winui_n2n/blob/main/LICENSE) file for details.
+
+## Author
+
+For more information, please visit the author's website: [Cialloo](https://www.cialloo.com)  
+For any inquiries, contact: [admin@cialloo.com](mailto:admin@cialloo.com)
+''';
 
 const _zhContent = '''
 项目基于 n2n , 一款优秀的跨平台开源p2p VPN软件  
